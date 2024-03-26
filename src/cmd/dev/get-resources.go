d375dd5019ae9de1cfb194ee6a922a6c622d8396
Megan Wolf <megan@defenseunicorns.com>
2024-03-26 10:33:13 -0400

    added tests, updated get-resources functionality

--- src/cmd/dev/get-resources.go
+++ src/cmd/dev/get-resources.go
@@ -2,7 +2,6 @@ package dev
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"os"
 
@@ -44,36 +43,13 @@ func init() {
 
 			ctx := context.Background()
 
-			validationFile, err := os.ReadFile(opts.InputFile)
+			collection, err := DevGetResources(ctx, opts.InputFile)
 			if err != nil {
-				message.Fatalf(err, "error reading YAML file: %v", err)
-			}
-
-			var validation types.Validation
-			err = yaml.Unmarshal(validationFile, &validation)
-			if err != nil {
-				message.Fatalf(err, "error unmarshaling YAML: %v", err)
-			}
-
-			var collection map[string]interface{}
-			// Get resources from the validation manifest -> refactor this?
-			if validation.Target.Domain == "kubernetes" {
-				payload := validation.Target.Payload
-
-				err := kube.EvaluateWait(payload.Wait)
-				if err != nil {
-					message.Fatalf(err, "error getting resources with wait: %v", err)
-				}
-
-				collection, err = kube.QueryCluster(ctx, payload.Resources)
-				if err != nil {
-					message.Fatalf(err, "error getting resources: %v", err)
-				}
+				message.Fatalf(err, "error running dev get-resources: %v", err)
 			}
 
 			PrintJSON(collection, opts.OutputFile)
 
-			// message.Infof("Successfully got resources %s is valid OSCAL version %s %s\n", opts.InputFile, validationResp.Validator.GetSchemaVersion(), validationResp.Validator.GetModelType())
 			spinner.Success()
 		},
 	}
@@ -84,21 +60,58 @@ func init() {
 	getResourcesCmd.Flags().StringVarP(&opts.OutputFile, "output-file", "o", "", "the path to write the resources json")
 }
 
-func PrintJSON(data map[string]interface{}, filepath string) {
-	// Marshal the data into JSON bytes.
-	jsonData, err := json.MarshalIndent(data, "", "  ")
+func DevGetResources(ctx context.Context, inputFile string) (map[string]interface{}, error) {
+	validationFile, err := os.ReadFile(inputFile)
 	if err != nil {
-		message.Fatalf(err, "error marshaling data to JSON: %v", err)
+		return nil, fmt.Errorf("error reading YAML file: %v", err)
 	}
+
+	var validation types.Validation
+	err = yaml.Unmarshal(validationFile, &validation)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshaling YAML: %v", err)
+	}
+
+	collection, err := GetResources(ctx, validation.Target)
+	if err != nil {
+		return nil, fmt.Errorf("error getting resources: %v", err)
+	}
+
+	return collection, nil
+}
+
+func PrintJSON(data map[string]interface{}, filepath string) {
+	jsonData := message.JSONValue(data)
 
 	// If a filepath is provided, write the JSON data to the file.
 	if filepath != "" {
-		err = os.WriteFile(filepath, jsonData, 0644)
+		err := os.WriteFile(filepath, []byte(jsonData), 0644)
 		if err != nil {
 			message.Fatalf(err, "error writing JSON to file: %v", err)
 		}
 	} else {
-		// Print to stdout
-		fmt.Println(string(jsonData))
+		// Else print to stdout
+		fmt.Println(jsonData)
 	}
+}
+
+// TODO: refactor this to a common package
+func GetResources(ctx context.Context, data types.Target) (map[string]interface{}, error) {
+	if data.Domain == "kubernetes" {
+		payload := data.Payload
+
+		err := kube.EvaluateWait(payload.Wait)
+		if err != nil {
+			return nil, err
+		}
+
+		collection, err := kube.QueryCluster(ctx, payload.Resources)
+		if err != nil {
+			return nil, err
+		}
+
+		return collection, nil
+	}
+
+	return nil, fmt.Errorf("domain not supported: %s", data.Domain)
 }
