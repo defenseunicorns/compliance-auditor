1c44236e0f3435b87ddb60ab7d83bb3c9f06536e
Cole (Mike) Winberry <cole@defenseunicorns.com>
2024-04-02 09:01:52 -0700

    docs: #70 update dev validate cmd comments and cli output.

--- src/cmd/dev/validate.go
+++ src/cmd/dev/validate.go
@@ -31,11 +31,11 @@ var validateOpts = &ValidateFlags{}
 func init() {
 	validateCmd := &cobra.Command{
 		Use:   "validate",
-		Short: "Validate Resources from a Lula Validation Manifest",
+		Short: "Run an individual Lula validation.",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			config.SkipLogFile = true
 		},
-		Long:    "TODO: Add long description here.",
+		Long:    "Run an individual Lula validation for quick testing and debugging of a Lula Validation. This command is intended for development purposes only.",
 		Example: validateHelp,
 		Run: func(cmd *cobra.Command, args []string) {
 			spinner := message.NewProgressSpinner("Validating %s", validateOpts.InputFile)
@@ -48,15 +48,19 @@ func init() {
 				message.Fatalf(err, "error running dev validate: %v", err)
 			}
 
+			// Write the validation result to a file if an output file is provided
+			// Otherwise, print the result to the debug console
 			err = writeValidation(validation, validateOpts.OutputFile)
 			if err != nil {
 				message.Fatalf(err, "error writing result: %v", err)
 			}
 
 			result := validation.Result.Failing == 0
+			// If the expected result is not equal to the actual result, return an error
 			if validateOpts.ExpectedResult != result {
 				message.Fatalf(fmt.Errorf("validation failed"), "expected result to be %t got %t", validateOpts.ExpectedResult, result)
 			}
+			// Print the number of passing and failing results
 			message.Infof("Validation completed with %d passing and %d failing results", validation.Result.Passing, validation.Result.Failing)
 		},
 	}
@@ -65,14 +69,14 @@ func init() {
 
 	validateCmd.Flags().StringVarP(&validateOpts.InputFile, "input-file", "f", "", "the path to a validation manifest file")
 	validateCmd.Flags().StringVarP(&validateOpts.OutputFile, "output-file", "o", "", "the path to write the validation with results")
-	validateCmd.Flags().BoolVarP(&validateOpts.ExpectedResult, "expected-result", "e", true, "the expected result of the validation")
+	validateCmd.Flags().BoolVarP(&validateOpts.ExpectedResult, "expected-result", "e", true, "the expected result of the validation (-e=false for failing result)")
 
 	validateCmd.MarkFlagRequired("input-file")
 }
 
 // DevValidate runs a validation using a lula validation manifest instead of an oscal file.
-// It returns the validation result.
-// or an error if the validation fails.
+// Successful if the validation is evaluated. Returns the validation result.
+// Returns an error if it fails to run the validation.
 func DevValidate(ctx context.Context, inputFile string) (validation types.Validation, err error) {
 	// Return an error if the input file is empty or not a yaml file
 	if inputFile == "" || !strings.HasSuffix(inputFile, ".yaml") {
