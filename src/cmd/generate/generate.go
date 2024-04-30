4f80fcaaef38d56ecb7453577fc5fa0bac5e1d1f
Brandt Keller <brandt.keller@defenseunicorns.com>
2024-04-30 20:41:52 +0000

    feat(generate): Cleanup the generate cmd file

--- src/cmd/generate/generate.go
+++ src/cmd/generate/generate.go
@@ -15,11 +15,6 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
-// The generate command is expected to have multiple different use-cases based upon
-// the artifact to be generated. Some opinionation will be involved - such as OSCAL
-// for the supported OSCAL models. This leaves generate open to generating other
-// artifacts in the future that support assessment and accreditation processes.
-
 type flags struct {
 	InputFile  string // -f --input-file
 	OutputFile string // -o --output-file
@@ -38,7 +33,7 @@ type componentFlags struct {
 var opts = &flags{}
 var componentOpts = &componentFlags{}
 
-// Base generate command will handle a large E2E focused generation that is driven from various artifacts.
+// Base generate command could handle a large E2E focused generation that is driven from various artifacts.
 // This will include some prerequisites such as component-definitions - but will ultimately lead to generation
 // of the SSP / SAP / POAM / and maybe SAR in a maintainable way.
 var generateCmd = &cobra.Command{
@@ -60,33 +55,38 @@ var generateComponentCmd = &cobra.Command{
 		var remarks []string
 		var title = "Component Title"
 		var outputFile = "oscal-component.yaml"
-		// check for inputFile flag content
+		// check for Catalog Source - this field is required
 		if componentOpts.CatalogSource == "" {
 			message.Fatal(fmt.Errorf("no catalog source provided"), "generate component requires a catalog input source")
 		}
 
+		// Assign remarks from flag or default to "statement"
 		if len(componentOpts.Remarks) == 0 {
 			remarks = []string{"statement"}
 		} else {
 			remarks = componentOpts.Remarks
 		}
 
+		// Assign the component title from flag or default to "Component Title"
 		if componentOpts.Component != "" {
 			title = componentOpts.Component
 		}
 
 		source := componentOpts.CatalogSource
 
+		// Fetch the catalog source
 		data, err := network.Fetch(source)
 		if err != nil {
 			message.Fatalf(fmt.Errorf("error fetching catalog source"), "error fetching catalog source")
 		}
 
+		// Create new catalog object
 		catalog, err := oscal.NewCatalog(source, data)
 		if err != nil {
 			message.Fatalf(fmt.Errorf("error creating catalog"), "error creating catalog")
 		}
 
+		// Create a component definition from the catalog given required context
 		comp, err := oscal.ComponentFromCatalog(source, catalog, title, componentOpts.Requirements, remarks, componentOpts.All)
 		if err != nil {
 			message.Fatalf(fmt.Errorf("error creating component"), "error creating component")
@@ -102,16 +102,18 @@ var generateComponentCmd = &cobra.Command{
 		if componentOpts.OutputFile != "" {
 			outputFile = componentOpts.OutputFile
 		}
+		// Check for an existing file
 		if _, err := os.Stat(outputFile); err == nil {
-			// if the file exists, we need to read it into bytes
 			existingFileBytes, err := os.ReadFile(outputFile)
 			if err != nil {
 				message.Fatalf(fmt.Errorf("error reading existing file"), "error reading existing file")
 			}
+			// Create new component definition object
 			existingComponent, err := oscal.NewOscalComponentDefinition(outputFile, existingFileBytes)
 			if err != nil {
 				message.Fatalf(fmt.Errorf("error creating new component definition"), "error creating new component definition")
 			}
+			// Merge the newly generated document into the existing document
 			comp, err = oscal.MergeComponentDefinitions(existingComponent, comp)
 			if err != nil {
 				message.Fatalf(fmt.Errorf("error merging component definition on component"), "error merging component definition on component")
@@ -129,69 +131,69 @@ var generateComponentCmd = &cobra.Command{
 		yamlEncoder.SetIndent(2)
 		yamlEncoder.Encode(component)
 
-		message.Infof("Writing Security Assessment Results to: %s", fileName)
+		message.Infof("Writing Component Definition to: %s", fileName)
 
 		err = os.WriteFile(fileName, b.Bytes(), 0644)
 		if err != nil {
-			message.Fatalf(fmt.Errorf("error writing Security Assessment Results to: %s", fileName), "error writing Security Assessment Results to: %s", fileName)
+			message.Fatalf(fmt.Errorf("error writing Component Definition to: %s", fileName), "error writing Security Assessment Results to: %s", fileName)
 		}
 
 	},
 }
 
-var generateAssessmentPlanCmd = &cobra.Command{
-	Use:     "assessment-plan",
-	Aliases: []string{"ap"},
-	Args:    cobra.MaximumNArgs(1),
-	Short:   "Generate an assessment plan OSCAL template",
-	Run: func(_ *cobra.Command, args []string) {
-		message.Info("generate assessment-plan executed")
+// var generateAssessmentPlanCmd = &cobra.Command{
+// 	Use:     "assessment-plan",
+// 	Aliases: []string{"ap"},
+// 	Args:    cobra.MaximumNArgs(1),
+// 	Short:   "Generate an assessment plan OSCAL template",
+// 	Run: func(_ *cobra.Command, args []string) {
+// 		message.Info("generate assessment-plan executed")
 
-		// For each component-definition in array of component-definitions
-		// Read component-definition
-		// Collect all implemented-requirements
-		// Collect all items from the backmatter
-		// Create new assessment-plan object
-		// Transfer to assessment-plan.reviewed-controls?
-	},
-}
+// 		// For each component-definition in array of component-definitions
+// 		// Read component-definition
+// 		// Collect all implemented-requirements
+// 		// Collect all items from the backmatter
+// 		// Create new assessment-plan object
+// 		// Transfer to assessment-plan.reviewed-controls?
+// 	},
+// }
 
-var generateSystemSecurityPlanCmd = &cobra.Command{
-	Use:     "system-security-plan",
-	Aliases: []string{"ssp"},
-	Args:    cobra.MaximumNArgs(1),
-	Short:   "Generate a system security plan OSCAL template",
-	Run: func(_ *cobra.Command, args []string) {
-		message.Info("generate system-security-plan executed")
+// var generateSystemSecurityPlanCmd = &cobra.Command{
+// 	Use:     "system-security-plan",
+// 	Aliases: []string{"ssp"},
+// 	Args:    cobra.MaximumNArgs(1),
+// 	Short:   "Generate a system security plan OSCAL template",
+// 	Run: func(_ *cobra.Command, args []string) {
+// 		message.Info("generate system-security-plan executed")
 
-		// For each component-definition in array of component-definitions
-		// Read component-definition
-		// Collect all implemented-requirements
-		// aggregate by control-id
-		// export to system-security-plan.control-implementation.implemented-requirements
-	},
-}
+// 		// For each component-definition in array of component-definitions
+// 		// Read component-definition
+// 		// Collect all implemented-requirements
+// 		// aggregate by control-id
+// 		// export to system-security-plan.control-implementation.implemented-requirements
+// 	},
+// }
 
-var generatePOAMCmd = &cobra.Command{
-	Use:     "plan-of-action-and-milestones",
-	Aliases: []string{"poam"},
-	Args:    cobra.MaximumNArgs(1),
-	Short:   "Generate a plan of actions and milestones OSCAL template",
-	Run: func(_ *cobra.Command, args []string) {
-		message.Info("generate plan-of-action-and-milestones executed")
+// var generatePOAMCmd = &cobra.Command{
+// 	Use:     "plan-of-action-and-milestones",
+// 	Aliases: []string{"poam"},
+// 	Args:    cobra.MaximumNArgs(1),
+// 	Short:   "Generate a plan of actions and milestones OSCAL template",
+// 	Run: func(_ *cobra.Command, args []string) {
+// 		message.Info("generate plan-of-action-and-milestones executed")
 
-		// Locate an assessment-results artifact
-		// Create an assessment-results object
-		// Transfer 'not-satisfied' findings and observations to poam.findings and poam.observations as appropriate
-	},
-}
+// 		// Locate an assessment-results artifact
+// 		// Create an assessment-results object
+// 		// Transfer 'not-satisfied' findings and observations to poam.findings and poam.observations as appropriate
+// 	},
+// }
 
 func GenerateCommand() *cobra.Command {
 
 	generateCmd.AddCommand(generateComponentCmd)
-	generateCmd.AddCommand(generateAssessmentPlanCmd)
-	generateCmd.AddCommand(generateSystemSecurityPlanCmd)
-	generateCmd.AddCommand(generatePOAMCmd)
+	// generateCmd.AddCommand(generateAssessmentPlanCmd)
+	// generateCmd.AddCommand(generateSystemSecurityPlanCmd)
+	// generateCmd.AddCommand(generatePOAMCmd)
 
 	generateFlags()
 	generateComponentFlags()
