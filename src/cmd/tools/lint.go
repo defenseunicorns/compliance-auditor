17e26dad93f06d921a8144d0a4d4bcfabe69dd4a
Andy Mills <andy@defenseunicorns.com>
2024-06-14 07:51:20 +0000

    updated to handle errors without exiting.

--- src/cmd/tools/lint.go
+++ src/cmd/tools/lint.go
@@ -29,31 +29,49 @@ func init() {
 		Long:    "Validate OSCAL documents are properly configured against the OSCAL schema",
 		Example: lintHelp,
 		Run: func(cmd *cobra.Command, args []string) {
+			var validationResults []validation.ValidationResult
+			var errorsOccurred bool
+
 			for _, inputFile := range opts.InputFiles {
-				spinner := message.NewProgressSpinner("Linting %s", inputFile)
+				spinner := message.NewProgressSpinner("Linting %s\n", inputFile)
 				defer spinner.Stop()
 
 				validationResp, err := validation.ValidationCommand(inputFile)
+
+
+				if err != nil {
+					message.Warnf("Failed to lint %s: %v\n", inputFile, err)
+					errorsOccurred = true
+					continue
+				}
 
 				for _, warning := range validationResp.Warnings {
 					message.Warn(warning)
 				}
 
-				if opts.ResultFile != "" {
-					validation.WriteValidationResult(validationResp.Result, opts.ResultFile)
-				}
+				validationResults = append(validationResults, validationResp.Result)
 
-				if err != nil {
-					message.Fatalf(err, "Failed to lint %s", inputFile)
-				}
 				message.Infof("Successfully validated %s is valid OSCAL version %s %s\n", inputFile, validationResp.Validator.GetSchemaVersion(), validationResp.Validator.GetModelType())
 				spinner.Success()
+			}
+
+			if opts.ResultFile != "" {
+				err := validation.WriteValidationResults(validationResults, opts.ResultFile)
+				if err != nil {
+					message.Fatalf(err, "Failed to write validation results to %s\n", opts.ResultFile)
+				}
+			}
+
+			if errorsOccurred {
+				message.Fatalf(nil, "Some files failed to lint. Check the error messages above.\n")
+			} else {
+				message.Infof("All files successfully validated.\n")
 			}
 		},
 	}
 
 	toolsCmd.AddCommand(lintCmd)
 
-	lintCmd.Flags().StringSliceVarP(&opts.InputFiles, "input-files", "f", []string{}, "the paths to oscal files")
+	lintCmd.Flags().StringSliceVarP(&opts.InputFiles, "input-files", "f", []string{}, "the paths to oscal json schema files")
 	lintCmd.Flags().StringVarP(&opts.ResultFile, "result-file", "r", "", "the path to write the validation result")
 }
