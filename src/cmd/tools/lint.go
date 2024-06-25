e1c90673cad7788881358deae657e2f642bfb033
Andy Mills <andy@defenseunicorns.com>
2024-06-25 00:15:42 +0000

    fixed my loop logic

--- src/cmd/tools/lint.go
+++ src/cmd/tools/lint.go
@@ -10,15 +10,15 @@ import (
 )
 
 type flags struct {
-	InputFile  string // -f --input-file
-	ResultFile string // -r --result-file
+	InputFiles []string // -f --input-files
+	ResultFile string   // -r --result-file
 }
 
 var opts = &flags{}
 
 var lintHelp = `
-To lint an existing OSCAL file:
-	lula tools lint -f <path to oscal>
+To lint existing OSCAL files:
+	lula tools lint -f <path1>,<path2>,<path3>
 `
 
 func init() {
@@ -28,42 +28,54 @@ func init() {
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
 			config.SkipLogFile = true
 		},
-		Long:    "Validate an OSCAL document is properly configured against the OSCAL schema",
+		Long:    "Validate OSCAL documents are properly configured against the OSCAL schema",
 		Example: lintHelp,
 		Run: func(cmd *cobra.Command, args []string) {
-			spinner := message.NewProgressSpinner("Linting %s", opts.InputFile)
-			defer spinner.Stop()
-
-			validationResp, err := validation.ValidationCommand(opts.InputFile)
-			// fatal for non-validation errors
-			if err != nil {
-				message.Fatalf(err, "Failed to lint %s: %s", opts.InputFile, err)
+			if len(opts.InputFiles) == 0 {
+				message.Fatalf(nil, "No input files specified")
 			}
 
-			for _, warning := range validationResp.Warnings {
-				message.Warn(warning)
-			}
+			for _, inputFile := range opts.InputFiles {
+				spinner := message.NewProgressSpinner("Linting %s", inputFile)
+				defer spinner.Stop()
 
-			if opts.ResultFile != "" {
-				validation.WriteValidationResult(validationResp.Result, opts.ResultFile)
-			} else {
-				jsonBytes, err := json.MarshalIndent(validationResp.Result, "", "  ")
+				validationResp, err := validation.ValidationCommand(inputFile)
+				// fatal for non-validation errors
 				if err != nil {
-					message.Fatalf(err, "Failed to marshal validation result")
+					message.Fatalf(err, "Failed to lint %s: %s", inputFile, err)
 				}
-				message.Infof("Validation result: %s", string(jsonBytes))
-			}
 
-			if validationResp.JsonSchemaError != nil {
-				message.Fatalf(err, "Failed to lint %s", opts.InputFile)
+				for _, warning := range validationResp.Warnings {
+					message.Warn(warning)
+				}
+
+				if opts.ResultFile != "" {
+					validation.WriteValidationResult(validationResp.Result, opts.ResultFile)
+				} else {
+					jsonBytes, err := json.MarshalIndent(validationResp.Result, "", "  ")
+					if err != nil {
+						message.Fatalf(err, "Failed to marshal validation result")
+					}
+					message.Infof("Validation result for %s: %s", inputFile, string(jsonBytes))
+				}
+
+				if validationResp.JsonSchemaError != nil {
+					message.Fatalf(err, "Failed to lint %s", inputFile)
+				}
+
+				// New conditional for logging success or failed linting
+				if validationResp.Result.Valid {
+					message.Infof("Successfully validated %s is valid OSCAL version %s %s\n", inputFile, validationResp.Validator.GetSchemaVersion(), validationResp.Validator.GetModelType())
+					spinner.Success()
+				} else {
+					message.Fatalf(nil, "Failed linting for %s", inputFile)
+				}
 			}
-			message.Infof("Successfully validated %s is valid OSCAL version %s %s\n", opts.InputFile, validationResp.Validator.GetSchemaVersion(), validationResp.Validator.GetModelType())
-			spinner.Success()
 		},
 	}
 
 	toolsCmd.AddCommand(lintCmd)
 
-	lintCmd.Flags().StringVarP(&opts.InputFile, "input-file", "f", "", "the path to a oscal json schema file")
+	lintCmd.Flags().StringSliceVarP(&opts.InputFiles, "input-files", "f", []string{}, "the paths to oscal json schema files (comma-separated)")
 	lintCmd.Flags().StringVarP(&opts.ResultFile, "result-file", "r", "", "the path to write the validation result")
 }
