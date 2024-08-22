c1e4b7b9551982ed7a4769312b1675c1eaf525ef
Brandt Keller <brandt.keller@defenseunicorns.com>
2024-08-22 22:53:32 +0000

    fix(oscal): ensure component definition UUID is updated on modification

--- src/pkg/common/oscal/complete-schema.go
+++ src/pkg/common/oscal/complete-schema.go
@@ -111,26 +111,39 @@ func MergeOscalModels(existingModel *oscalTypes_1_1_2.OscalModels, newModel *osc
 
 	// Component definition
 	if modelType == "component" {
+
+		if existingModel.ComponentDefinition == nil && newModel.ComponentDefinition == nil {
+			return nil, fmt.Errorf("Cannot merge nil component-definitions")
+		} else if existingModel.ComponentDefinition == nil && newModel.ComponentDefinition != nil {
+			existingModel.ComponentDefinition = newModel.ComponentDefinition
+			return existingModel, nil
+		}
+		// Is there a scenario where the new content could be nil?
+
 		merged, err := MergeComponentDefinitions(existingModel.ComponentDefinition, newModel.ComponentDefinition)
 		if err != nil {
 			return nil, err
 		}
 		// Re-assign after processing errors
 		existingModel.ComponentDefinition = merged
-	} else if existingModel.ComponentDefinition == nil && newModel.ComponentDefinition != nil {
-		existingModel.ComponentDefinition = newModel.ComponentDefinition
 	}
 
 	// Assessment Results
 	if modelType == "assessment-results" {
+
+		if existingModel.AssessmentResults == nil && newModel.AssessmentResults == nil {
+			return nil, fmt.Errorf("Cannot merge nil assessment-results")
+		} else if existingModel.AssessmentResults == nil && newModel.AssessmentResults != nil {
+			existingModel.AssessmentResults = newModel.AssessmentResults
+			return existingModel, nil
+		}
+
 		merged, err := MergeAssessmentResults(existingModel.AssessmentResults, newModel.AssessmentResults)
 		if err != nil {
 			return existingModel, err
 		}
 		// Re-assign after processing errors
 		existingModel.AssessmentResults = merged
-	} else if existingModel.AssessmentResults == nil && newModel.AssessmentResults != nil {
-		existingModel.AssessmentResults = newModel.AssessmentResults
 	}
 
 	return existingModel, err
