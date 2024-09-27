d707143ebea930834bb954de082239f1200c82dc
Andy Mills <andy@defenseunicorns.com>
2024-09-27 13:44:17 +0000

    fix e2e test

--- src/test/e2e/report_test.go
+++ src/test/e2e/report_test.go
@@ -2,48 +2,47 @@ package test
 
 import (
 	"bytes"
-	"os/exec"
 	"testing"
 
+	"github.com/defenseunicorns/lula/src/cmd"
 	"github.com/defenseunicorns/lula/src/pkg/message"
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	validMultiComponentPath = "../unit/common/oscal/valid-multi-component.yaml"
-	catalogPath             = "../unit/common/oscal/catalog.yaml"
-)
-
-// TestLulaReportValidComponent checks that the 'lula report' command works with a valid component definition.
+// TestLulaReportValidComponent checks that the 'lula report' command works with a valid component definition with multiple components and framework prop.
 func TestLulaReportValidComponent(t *testing.T) {
 	// Disable progress indicators and other extra formatting
 	message.NoProgress = true
 
+	// Setup the root command and buffers for capturing output
+	rootCmd := cmd.RootCommand()
+	rootCmd.SetArgs([]string{"report", "-f", "../unit/common/oscal/valid-multi-component.yaml", "--file-format", "table"})
+
 	var outBuf, errBuf bytes.Buffer
+	rootCmd.SetOut(&outBuf)
+	rootCmd.SetErr(&errBuf)
 
-	cmd := exec.Command("lula", "report", "-f", validMultiComponentPath, "--file-format", "table")
-	cmd.Stdout = &outBuf
-	cmd.Stderr = &errBuf
-
-	err := cmd.Run()
+	// Execute the command
+	err := rootCmd.Execute()
 
 	// Check for errors in command execution.
-	assert.NoError(t, err, "Expected no error from `lula report` with valid component definition")
-}
+	assert.NoError(t, err, "Expected no error from `lula report` with valid component definition")}
 
-// TestLulaReportCatalog checks that the 'lula report' command fails gracefully with a catalog file.
-// OSCAL Catalogs are not currently supported by 'lula report' command yet.
+// TestLulaReportCatalog checks that the 'lula report' command fails gracefully with a catalog file since it is not currently supported.
+// TODO: This test will need to be changed as more models are supported for reporting.
 func TestLulaReportCatalog(t *testing.T) {
 	// Disable progress indicators and other extra formatting
 	message.NoProgress = true
 
+	// Setup the root command and buffers for capturing output
+	rootCmd := cmd.RootCommand()
+	rootCmd.SetArgs([]string{"report", "-f", "../unit/common/oscal/catalog.yaml", "--file-format", "table"})
+
 	var outBuf, errBuf bytes.Buffer
+	rootCmd.SetOut(&outBuf)
+	rootCmd.SetErr(&errBuf)
 
-	cmd := exec.Command("lula", "report", "-f", catalogPath, "--file-format", "table")
-	cmd.Stdout = &outBuf
-	cmd.Stderr = &errBuf
+	// Execute the command
+	err := rootCmd.Execute()
 
-	err := cmd.Run()
-
-	assert.Error(t, err, "error running report")
-}
+	assert.Error(t, err, "Expected an error running report")}
