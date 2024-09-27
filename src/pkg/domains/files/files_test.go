47fa21322dd5445a9d6b62987fae26d7ed0be601
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2024-09-27 13:43:28 -0400

    adding remote file download and some okayish tests

--- src/pkg/domains/files/files_test.go
+++ src/pkg/domains/files/files_test.go
@@ -1,5 +1,29 @@
 package files
 
-import "github.com/defenseunicorns/lula/src/types"
+import (
+	"testing"
+
+	"github.com/defenseunicorns/lula/src/types"
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/require"
+)
 
 var _ types.Domain = (*Domain)(nil)
+
+func TestGetResource(t *testing.T) {
+	t.Run("local files", func(t *testing.T) {
+		d := Domain{Spec: &Spec{Filepaths: []string{
+			"testdata/foo.yaml",
+			"testdata/bar.json",
+		}}}
+
+		resources, err := d.GetResources()
+		require.NoError(t, err)
+		if diff := cmp.Diff(resources, types.DomainResources{"bar.json": map[string]interface{}{"cat": "Cheetarah"}, "foo.yaml": "cat = Li Shou"}); diff != "" {
+			t.Fatalf("wrong result:\n%s\n", diff)
+		}
+	})
+
+	//remote files
+	// TODO
+}
