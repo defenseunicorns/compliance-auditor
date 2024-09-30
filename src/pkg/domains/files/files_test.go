9448d77158d2a8a510a9f0b18bd13c0d3071c057
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2024-09-30 11:43:05 -0400

    fix use of filepath.Name

--- src/pkg/domains/files/files_test.go
+++ src/pkg/domains/files/files_test.go
@@ -15,15 +15,19 @@ func TestGetResource(t *testing.T) {
 		d := Domain{Spec: &Spec{Filepaths: []FileInfo{
 			{Name: "foo.yaml", Path: "testdata/foo.yaml"},
 			{Name: "bar.json", Path: "testdata/bar.json"},
+			{Name: "arbitraryname", Path: "testdata/nested-directory/baz.hcl2"},
 		}}}
 
 		resources, err := d.GetResources()
 		require.NoError(t, err)
-		if diff := cmp.Diff(resources, types.DomainResources{"bar.json": map[string]interface{}{"cat": "Cheetarah"}, "foo.yaml": "cat = Li Shou"}); diff != "" {
+		if diff := cmp.Diff(resources, types.DomainResources{
+			"bar.json": map[string]interface{}{"cat": "Cheetarah"},
+			"foo.yaml": "cat = Li Shou",
+			"arbitraryname": map[string]any{
+				"resource": map[string]any{"catname": map[string]any{"blackcat": map[string]any{"name": "robin"}}},
+			},
+		}); diff != "" {
 			t.Fatalf("wrong result:\n%s\n", diff)
 		}
 	})
-
-	//remote files
-	// TODO
 }
