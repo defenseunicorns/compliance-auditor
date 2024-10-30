18e2b37d9e7883a1b36184cd5deb36fc1e4f674e
Megan Wolf <megan@defenseunicorns.com>
2024-10-30 09:27:37 -0400

    fix: re-orged print commands

--- src/cmd/dev/print_test.go
+++ src/cmd/dev/print_test.go
@@ -12,10 +12,15 @@ import (
 	"github.com/defenseunicorns/lula/src/types"
 )
 
+const (
+	assessmentPath = "../../test/unit/common/oscal/valid-assessment-results-with-resources.yaml"
+	componentPath  = "../../test/unit/common/oscal/valid-multi-component-validations.yaml"
+)
+
 func TestPrintResources(t *testing.T) {
 	t.Parallel()
 
-	oscalModel := testhelpers.OscalFromPath(t, "../../test/unit/common/oscal/valid-assessment-results-with-resources.yaml")
+	oscalModel := testhelpers.OscalFromPath(t, assessmentPath)
 	assessment := oscalModel.AssessmentResults
 	require.NotNil(t, assessment)
 
@@ -53,6 +58,47 @@ func TestPrintResources(t *testing.T) {
 	t.Run("Test print resources with no resources", func(t *testing.T) {
 		err := dev.PrintResources(assessment, "af060637-2899-4f26-ae9d-2c1bbbddc4b0", "../../test/unit/common/oscal", "")
 		require.ErrorContains(t, err, "observation does not contain a remote reference")
+	})
+
+}
+
+func TestPrintValidation(t *testing.T) {
+	t.Parallel()
+
+	oscalAssessmentModel := testhelpers.OscalFromPath(t, assessmentPath)
+	assessment := oscalAssessmentModel.AssessmentResults
+	require.NotNil(t, assessment)
+
+	oscalComponentModel := testhelpers.OscalFromPath(t, componentPath)
+	component := oscalComponentModel.ComponentDefinition
+	require.NotNil(t, component)
+
+	t.Run("Test print validation", func(t *testing.T) {
+		tmpFile := testhelpers.CreateTempFile(t, ".json")
+		defer os.Remove(tmpFile.Name())
+
+		err := dev.PrintValidation(component, assessment, "92cb3cad-bbcd-431a-aaa9-cd47275a3982", tmpFile.Name())
+		require.NoError(t, err)
+
+		// get printed data
+		printedData, err := os.ReadFile(tmpFile.Name())
+		require.NoError(t, err)
+
+		// get actual data
+		validationData, err := os.ReadFile("../../test/unit/common/validation/validation.resource-print.yaml")
+		require.NoError(t, err)
+
+		require.Equal(t, validationData, printedData)
+	})
+
+	t.Run("Test print validation with no validation prop", func(t *testing.T) {
+		err := dev.PrintValidation(component, assessment, "e1ca2968-8652-41be-a19f-c32bc0b3086c", "")
+		require.ErrorContains(t, err, "no validation linked to observation")
+	})
+
+	t.Run("Test print resources with validation not in backmatter", func(t *testing.T) {
+		err := dev.PrintValidation(component, assessment, "af060637-2899-4f26-ae9d-2c1bbbddc4b0", "")
+		require.ErrorContains(t, err, "validation not found in component definition")
 	})
 
 }
--- src/test/e2e/cmd/dev_print_test.go
+++ src/test/e2e/cmd/dev_print_test.go
@@ -9,23 +9,47 @@ import (
 	"github.com/defenseunicorns/lula/src/pkg/message"
 )
 
-func TestDevPrintValidationCommand(t *testing.T) {
+func TestDevPrintResourcesCommand(t *testing.T) {
 	message.NoProgress = true
 
 	test := func(t *testing.T, args ...string) error {
-		rootCmd := dev.PrintValidationCommand()
+		rootCmd := dev.PrintCommand()
 
 		return runCmdTest(t, rootCmd, args...)
 	}
 
 	testAgainstGolden := func(t *testing.T, goldenFileName string, args ...string) error {
-		rootCmd := dev.PrintValidationCommand()
+		rootCmd := dev.PrintCommand()
 
-		return runCmdTestWithGolden(t, "dev/print-validation/", goldenFileName, rootCmd, args...)
+		return runCmdTestWithGolden(t, "dev/print/", goldenFileName, rootCmd, args...)
 	}
 
+	t.Run("Print Resources", func(t *testing.T) {
+		err := testAgainstGolden(t, "resources", "--resources",
+			"-a", "../../unit/common/oscal/valid-assessment-results-with-resources.yaml",
+			"-u", "92cb3cad-bbcd-431a-aaa9-cd47275a3982",
+		)
+		require.NoError(t, err)
+	})
+
+	t.Run("Print Resources - invalid oscal", func(t *testing.T) {
+		err := test(t, "--resources",
+			"-a", "../../unit/common/validation/validation.opa.yaml",
+			"-u", "92cb3cad-bbcd-431a-aaa9-cd47275a3982",
+		)
+		require.ErrorContains(t, err, "error creating oscal assessment results model")
+	})
+
+	t.Run("Print Resources - no uuid", func(t *testing.T) {
+		err := test(t, "--resources",
+			"-a", "../../unit/common/oscal/valid-assessment-results-with-resources.yaml",
+			"-u", "foo",
+		)
+		require.ErrorContains(t, err, "error printing resources")
+	})
+
 	t.Run("Print Validation", func(t *testing.T) {
-		err := testAgainstGolden(t, "validation",
+		err := testAgainstGolden(t, "validation", "--validation",
 			"-a", "../../unit/common/oscal/valid-assessment-results-with-resources.yaml",
 			"-c", "../../unit/common/oscal/valid-multi-component-validations.yaml",
 			"-u", "92cb3cad-bbcd-431a-aaa9-cd47275a3982",
@@ -34,7 +58,7 @@ func TestDevPrintValidationCommand(t *testing.T) {
 	})
 
 	t.Run("Print Validation - invalid assessment oscal", func(t *testing.T) {
-		err := test(t,
+		err := test(t, "--validation",
 			"-a", "../../unit/common/validation/validation.opa.yaml",
 			"-c", "../../unit/common/oscal/valid-multi-component-validations.yaml",
 			"-u", "92cb3cad-bbcd-431a-aaa9-cd47275a3982",
@@ -43,7 +67,7 @@ func TestDevPrintValidationCommand(t *testing.T) {
 	})
 
 	t.Run("Print Validation - no uuid", func(t *testing.T) {
-		err := test(t,
+		err := test(t, "--validation",
 			"-a", "../../unit/common/oscal/valid-assessment-results-with-resources.yaml",
 			"-c", "../../unit/common/oscal/valid-multi-component-validations.yaml",
 			"-u", "foo",
