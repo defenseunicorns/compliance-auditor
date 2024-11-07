49fb0bf7a4135ec5e6dbca140622bed4bdceeaaa
Kristin Laemmert <mildwonkey@users.noreply.github.com>
2024-11-07 11:04:01 -0500

    feat(api domain): accept any API response and cinlude the status code in the domain resources

--- src/pkg/domains/api/api.go
+++ src/pkg/domains/api/api.go
@@ -31,17 +31,19 @@ func (a ApiDomain) makeRequests(ctx context.Context) (types.DomainResources, err
 		defaultClient := clientFromOpts(defaultOpts)
 
 		for _, request := range a.Spec.Requests {
-			var responseType interface{}
+			var responseType map[string]interface{}
 			var err error
+			var status int
 			if request.Options == nil {
-				responseType, err = doHTTPReq(ctx, defaultClient, *request.reqURL, defaultOpts.Headers, request.reqParameters, responseType)
+				responseType, status, err = doHTTPReq(ctx, defaultClient, *request.reqURL, defaultOpts.Headers, request.reqParameters, responseType)
 			} else {
 				client := clientFromOpts(request.Options)
-				responseType, err = doHTTPReq(ctx, client, *request.reqURL, request.Options.Headers, request.reqParameters, responseType)
+				responseType, status, err = doHTTPReq(ctx, client, *request.reqURL, request.Options.Headers, request.reqParameters, responseType)
 			}
 			if err != nil {
 				return collection, err
 			}
+			responseType["status"] = status
 			collection[request.Name] = responseType
 		}
 		return collection, nil
